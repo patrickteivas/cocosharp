f0599ea8e7842bb2b61359a4db28b670ddf149de
Patrick Teivas <teivas.2011@mail.ru>
2019-05-23 09:11:17 +0300

    xd

---
+++ CocosSharpGame1/CocosSharpGame1/GameLayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using CocosSharp;
+namespace BouncingGame
+{
+    public class GameLayer : CCLayerColor
+    {
+        CCSprite paddleSprite;
+        public GameLayer() : base(CCColor4B.Black)
+        {
+            // "paddle" refers to the paddle.png image
+            paddleSprite = new CCSprite("paddle");
+            paddleSprite.PositionX = 100;
+            paddleSprite.PositionY = 100;
+            AddChild(paddleSprite);
+        }
+        protected override void AddedToScene()
+        {
+            base.AddedToScene();
+            // Use the bounds to layout the positioning of the drawable assets
+            CCRect bounds = VisibleBoundsWorldspace;
+            // Register for touch events
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+            touchListener.OnTouchesEnded = OnTouchesEnded;
+            AddEventListener(touchListener, this);
+        }
+        void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
+        {
+            if (touches.Count > 0)
+            {
+                // Perform touch handling here
+            }
+        }
+    }
+}
